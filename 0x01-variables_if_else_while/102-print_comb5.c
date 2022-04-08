bda35484dba040bcbee20ed9d63c5ac6f59c2f08
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-08 16:36:32 -0700

    My second commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,8 +14,6 @@ for (num1 = 0; num1 <= 98; num1++)
 {
 for (num2 = num1 + 1; num2 < 99; num2++)
 {
-for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-{
 putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
 putchar(' ');
