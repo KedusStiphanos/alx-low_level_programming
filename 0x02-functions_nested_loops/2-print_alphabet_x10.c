abe8fb31921858a8f7dc12a232957760e6f80a50
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-13 12:49:43 -0700

    My commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints
+ */
+void print_alphabet_x10(void)
+{
+int c=0;
+char l;
+while (c++ <= 9)
+{
+for (l = 'a'; l<= 'z'; l++)
+_putchar(l);
+_putchar('\n');
+}
+}
