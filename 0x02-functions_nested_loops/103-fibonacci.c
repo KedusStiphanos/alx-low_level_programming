6aea2c6db68a9db54ca52815480584c833ca4fbe
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-13 16:10:16 -0700

    My commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - list
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+unsigned long f1 = 0, f2 = 1, s;
+float ts;
+
+while (1)
+{
+	s = f1+ f2
+		if (s > 4000000)
+			break;
+	if ((s % 2) == 0)
+		ts += s;
+f1 = f2;
+f2 = s;
+}
+printf("%.0f\n",ts);
+
+return (0);
+}
