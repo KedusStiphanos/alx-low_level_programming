98c3f147d7cd4fe0bf3ca968447a7d59f198cabd
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-17 09:41:49 -0700

    task 1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * more_numbers - checkes if the no is b/n 0 to 9
+ * more_numbers - prints numbers from 0 to 14 10 times
  * 
  * Return: void
  */
 
 void more_numbers(void)
 {
-int i, j ,a;
+int i, j, a;
 for (a = 0; a < 10; a++)
 {
 for (i = 0; i <= 1; i++)
