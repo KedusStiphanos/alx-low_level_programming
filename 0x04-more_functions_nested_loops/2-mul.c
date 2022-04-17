9fba84c168959534e12f55e9ea15a9be77a99d3c
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-17 08:08:09 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two int
+ * @a: input one
+ * @b: input two
+ * Return: multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print _numbers - checkes if the no is b/n 0 to 9
+ * 
+ * Return: Always 0 (Success)
+ */
+
+void print-numbers(void)
+{
+	
+	int i;
+
+	i = 0;
+
+	for (i = 0; i < 10; i++)
+
+	_putchar(i + '0');
+
+	_putchar ('\n');
+}
