dec773b17025d2a4e0b35b06ba8316e465bbcf1a
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-17 09:07:07 -0700

    task 1

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,27 +1,24 @@
 #include "main.h"
-
 /**
- * print_most_numbers - checkes if the no is b/n 0 to 9
+ * print_most_numbers - checks the code
  * 
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
-
 void print_most_numbers(void)
 {
-	int i;
 
-	i = 0;
+int i;
 
-	for (i = 0; i < 10; i++)
-	{
+i = 0;
+while (i < 10);
+{
+if (i != 2 && i != 4)
+{
+_putchar (i + '0');
+}
+i++;
+}
 
-		if (i -- 2 || i -- 4)
-
-			i++;
-
-		_putchar(i + '0');
-
-	}
-		_putchar('\n');
+_putchar ('\n');
 
 }
