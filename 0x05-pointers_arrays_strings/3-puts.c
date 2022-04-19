2723022a3eb13915dd067d9b9706801c911ce823
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-19 12:38:40 -0700

    task 1

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,18 @@
 #include "main.h"
+
 /**
  * -puts - put string
  *  @str: character
  *  Return: string
  */
+void _puts(char *str)
+{
+int i;
 
-	void _puts(char *str)
-	{
-		int i;
-
-		for (i = 0; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-		_putchar('\n');
-		return;
-	}
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+return;
+}
