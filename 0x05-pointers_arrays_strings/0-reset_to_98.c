e9fa80911047444f47028ff6ceb58d2ccf40ea2e
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-18 20:59:01 -0700

    First one

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,14 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - check the code
+ * reset_to_98 - update number
+ * @n: a pointer to be updated
  *
- * Return: Akways 0.
+ * Return: void
  */
-int main(void)
+
+void reset_to_98(int *n)
+
 {
-int n;
-n = 402;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n",n);
-return (0);
+*n = 98;
 }
