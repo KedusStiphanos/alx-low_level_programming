c8ef3390a17d386b3445ed46442d59a90f520fc2
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-19 13:12:30 -0700

    task 1

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * rev_string - rev string
- * @S: to be reversed
+ * print_rev - rev string
+ * @s: to be reversed
  * Return: String
  */
-void rev_string(char *s)
+void print_rev(char *s)
 {
 	int i, j, k;
 j = 0;
-{
+
 for (i = 0; s[i] != '\0'; i++)
+{
 	j++;
 }
+for (k = j; k == 0; k--)
 {
-	for (k = j; k = 0; k = k - 1)
-	rev_string(s[j]);
+		_putchar(s[j]);
 }
+_putchar('\n');
 return;
 }
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - rev string
+ * @S: to be reversed
+ * Return: String
+ */
+void rev_string(char *s)
+{
+	int i, j, k;
+j = 0;
+{
+for (i = 0; s[i] != '\0'; i++)
+	j++;
+}
+{
+	for (k = j; k = 0; k = k - 1)
+	rev_string(s[j]);
+}
+return;
+}
