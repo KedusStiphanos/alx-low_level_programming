b97afccffd419047f78702634162eb630ac4b16e
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-18 21:32:48 -0700

    First one

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+
+{
+*a = 98;
+*b = 42;
+}
