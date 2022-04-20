ebb3eef9cf1df32a25fb5987d73629eac2b9698c
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-19 17:02:26 -0700

    task 1

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,19 @@ void print_array(int *a, int *n)
 int i;
 for (i = 0; i < n; i++)
 {
-printf("%d", a[i]);
-if (i < (n - 1))
+	printf("%d", a[i]);
+	if (i < (n - 1))
+	{
+		printf(", ");
+iy
+	i++;
+j = i / 2;
+if (i % 2)
+j += 1;
+while (j < i)
 {
-printf(", ");
+_putchar(str[j]);
+j++;
 }
-}
-printf('\n');}
+_putchar('\n');
 }
