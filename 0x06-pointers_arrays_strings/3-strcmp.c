9472f64b48521a1b3e4ccaafedffe0f0d1bc55a7
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-22 03:31:55 -0700

    Task 1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
  * _strcmp - write a functino that compares
+ *
  * @s1: Second string
  * @s2: first string
  *
- * Return: My return
+ * Return: If the strings are equal return "0", if not return other number
  */
 
-char _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++)
 ;
