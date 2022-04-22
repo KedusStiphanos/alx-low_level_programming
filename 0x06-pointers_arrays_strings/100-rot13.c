056edbc1462ba2c26ff8c0b45d16d922e7df930f
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-22 13:12:20 -0700

    Task 1

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,26 @@
 #include "main.h"
 /**
- * rot13 - write a functinon that encodes a string into 1337
- * 
- * @hi: This is input string
+ * rot13 - encodes strings using rot13
+ * @s: This is input string
  *
  * Return: String converted to 13
  *
  */
 
-char *rot13(char *hi)
+char *rot13(char *s)
 {
 int i, c;
 
-char m[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char n[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char m[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char n[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 
-for (i = 0; hi[i] != '\0'; ++i)
+for (i = 0; s[i] != '\0'; i++)
 {
 	for (c = 0; m[c] != '\0' ; c++)
 	{
-		if (hi[i] == m[c])
+		if (s[i] == m[c])
 		{
-			hi[i] = n[c];
+		        s[i] = n[c];
 			break;
 		}
 	}
