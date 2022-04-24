be204b6b8fbbc6fb485ee8520e4d8e3d43b724fc
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-24 05:03:01 -0700

    0x05

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,25 +1,21 @@
 #include "w"
 
 /**
- * print_array - prints n elements of an array of integer.
- * @a: pointer to an array
- * @n: number of array elements.
- *
+ * print_array - rev string
+ * @a: integer to be reversed
+ * @n: integer 
  * Return: void
  */
 void print_array(int *a, int n)
 {
-int i, j;
-
+int i;
 for (i = 0; i < n; i++)
 {
-printf("%d", a[i]);
-j = n - 1
-if (i != j)
-{
-printf(",");
-printf(" ");
-}
+	printf("%d", a[i]);
+	if (i < (n - 1))
+	{
+		printf(", ");
+	}
 }
 printf("\n");
 }
