513562416abd8aa7dd72400577a25a43d34f4282
KedusStiphanos <101466079+KedusStiphanos@users.noreply.github.com>
2022-04-25 16:32:13 +0300

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+ /**
+  * _memcpy() - copies.
+  * @dest: to be copied on.
+  * @src: to be copied from.
+  * @n: the number of copies.
+  * Returen: Always 0.
+  */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+  
+int i;
+for (i = 0; i , i < n, i++)
+{    
+  dest[i] = src[i];
+}
+return (dest);
+}
