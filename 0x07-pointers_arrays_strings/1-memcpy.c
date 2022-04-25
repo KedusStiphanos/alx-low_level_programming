e0f2117ba2102ea504fb849bc962568c0e572c59
KedusStiphanos <KedusStiphanos@gmail.com>
2022-04-25 07:09:47 -0700

    bashrc

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
- /**
+
+/**
  * char *_memcpy() - copies.
  * @dest: to be copied on.
  * @src: to be copied from.
  * @n: the number of copies.
  * Return: Always 0.
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
   
